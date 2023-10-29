d2922184e7fe8e676daa389e7a406aecdb67d64b
José Venturini <jose.venturini@useaurea.com>
2023-10-29 19:44:31 -0300

    threads :D

---
+++ Temp.py
@@ -0,0 +1,36 @@
+import time
+import threading
+
+class Temp:
+    def __init__(self, seconds):
+        self.seconds = seconds
+        self._thread = None
+        self._stop_event = threading.Event()
+
+    def _run(self):
+        while self.seconds and not self._stop_event.is_set():
+            mins, secs = divmod(self.seconds, 60)
+            timeformat = '{:02d}:{:02d}'.format(mins, secs)
+            print(timeformat, end='\r')
+            time.sleep(1)
+            self.seconds -= 1
+
+        if not self._stop_event.is_set():
+            if self.callback:
+                self.callback()
+
+    def start(self, callback=None):
+        self.callback = callback
+        if self._thread is None:
+            self._thread = threading.Thread(target=self._run)
+            self._thread.start()
+
+    def stop(self, callback=None):
+        if self._thread:
+            self._stop_event.set()
+            self._thread.join()  
+            self._thread = None
+            if callback:
+                callback()
+    def is_running(self):
+        return self._thread is not None
--- TokenRing.py
+++ TokenRing.py
@@ -1,19 +1,25 @@
 import threading
+from Temp import Temp
 from UDPSocket import UDPSocket
 import queue
-
 class TokenRing:
     def __init__(self):
         self.__UDPSocket = UDPSocket(5000)
         threading.Thread(target=self.__receive_data).start()
         self.__configure()
         self.__queue = queue.Queue()
+        self.__temp_token_management = Temp(5)
+        self.__temp_with_token = Temp(self.__token_time)
+        self.__token_holder_flag = False
+        self.__ack_event_thread = None
+        self.__ack_event = threading.Event()
+        self.__last_message = None
 
     def __receive_data(self):
         while True:
             data = self.__UDPSocket.recv()
             if data:
-                print(data.decode('utf-8'))
+                self.__decode_data(data.decode("utf-8"))
             else:
                 print('No data received')
     
@@ -24,10 +30,37 @@ class TokenRing:
             self.__right_port = int(data[0].split(':')[1].strip())
             self.__right_nickname = data[1].strip()
             self.__token_time = int(data[2].strip())
-            self.__token = bool(data[3].strip())
+            self.__token = data[3].strip() == 'true'
+
+    def __control_token(self):
+        def remove_token():
+            print('Token removed')
+
+        def token_timeout():
+            print('Token timeout')
+            self.__send_token()
+
+        def multiple_tokens():
+            remove_token()
+        
+        if self.__temp_token_management.is_running():
+            multiple_tokens()
+        else: 
+            self.__temp_token_management.start(token_timeout)
+            self.__is_token_holder = True
 
     def __decode_data(self, data):
-        pass
+        if data == '9000':
+            if (self.__token):
+                self.__control_token()
+            else:
+                self.__is_token_holder = True
+                #self.__temp_with_token.start(self.__send_token)
+        else:
+            msg = data.split(';')
+            ack = msg[0].split(':')[1]
+            if (ack == 'ACK' or ack == 'NACK' or 'naoexiste'):
+                self.__ack_event.set()
 
     def __calculate_crc(self, data):
         pass
@@ -38,31 +71,49 @@ class TokenRing:
     def __retransmit_message(self, data):
         pass
 
-    def __acknowledge_message(self, data):
-        pass
+    def __wait_for_acknowledgement(self):
+        self.__ack_event.wait()
+        print('ACK received')
+        self.__last_message = None
+        self.__ack_event.clear()
+
+    @property
+    def __is_token_holder(self):
+        return self.__token_holder_flag
+
+    @__is_token_holder.setter
+    def __is_token_holder(self, value):
+        if value and not self.__token_holder_flag: 
+            print('Token holder')
+            self.__token_holder_flag = value
+            self.__ack_event_thread = threading.Thread(target=self.__send_message_when_token_holder)
+            self.__ack_event_thread.start()
+
+    def __send_message_when_token_holder(self):
+        print('Sending message')
+        if not self.__queue.empty():
+            self.__send(self.__queue.get())
+            self.__wait_for_acknowledgement()
+            self.__temp_with_token.start(self.__send_token)
 
     def __send_token(self):
-        pass
-
-    def __remove_token(self):
-        pass
-
-    def __token_timeout_check(self):
-        pass
+        self.__is_token_holder = False
+        self.__send(b'9000')
 
     def __enqueue_message(self, message):
-        pass
+        self.__queue.put(message)
 
-    def __dequeue_message(self):
-        pass
         
     def __send(self, data):
+        print('Sending data', data)
+        self.__last_message = data
         self.__UDPSocket.send(data, (self.__right_ip, self.__right_port))
 
     def send_message(self, message, nickname):
-        pass
+        self.__enqueue_message(message)
 
     def introduce_error(self):
         pass
 
 tk = TokenRing()
+tk.send_message(b'7777:ACK;Bob:Mary;19385749;Oi pessoal!', 'Bob')
